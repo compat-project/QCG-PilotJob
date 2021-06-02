2ed0d42702638c45e28b2f1d11fd2eaaa5bc2a0b
Piotr Kopta <pkopta@man.poznan.pl>
2021-06-02 18:32:18 +0200

    bugfix

--- components/core/pytest-cov.sh
+++ components/core/pytest-cov.sh
@@ -8,5 +8,5 @@ rm -fR /srv/src/coverage.out
 mkdir /srv/src/coverage.out
 rm -fR /srv/src/htmlcov.report
 
-pytest -v --cov-config=/srv/src/coveragerc.ini --cov=qcg.pilotjob $* src/
+pytest -v --cov-config=/srv/src/coveragerc.ini --cov=qcg.pilotjob $*
 coverage html --rcfile=/srv/src/coveragerc.ini
--- run-slurm-docker-tests.sh
+++ run-slurm-docker-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-test_cmdline="/srv/src/pytest-cov.sh $*"
+test_cmdline="/srv/src/pytest-cov.sh $* src/"
 echo "test cmdline: $test_cmdline"
 
 docker exec -it slurmctld sbatch -N 2 --ntasks-per-node 2 --overcommit --oversubscribe -o /srv/src/slurm-tests-out.txt -D /srv --exclusive --wait --wrap="$test_cmdline"
